f364471f7873b1dd4fe4e50d7c1476406f28d2b8
Jernej <Jernej@ppc>
2019-01-20 22:04:15 +0100

    forma in localhost server

---
+++ testImpolRest/Controllers/calculationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using testImpolRest.Models;
+using rondAlgo;
+
+namespace testImpolRest.Controllers
+{
+    public class CalculationController : ApiController
+    {
+        Calculation cal = new Calculation();
+
+        public HttpResponseMessage Post(Parameter param)
+        {
+            List<Rond> result = cal.Calculate(param.length, param.width, param.radius, param.minTopDistance, param.minLeftDistance, param.minMiddleDistance);
+            int resultCount = result.Count;
+            var response = Request.CreateResponse<int>(System.Net.HttpStatusCode.Created, resultCount);
+            return response;
+        }
+    }
+}
---
+++ testImpolRest/Models/Parameter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testImpolRest.Models
+{
+    public class Parameter
+    {
+        public int length { get; set; }
+        public int width { get; set; }
+        public int radius { get; set; }
+        public int minTopDistance { get; set; }
+        public int minLeftDistance { get; set; }
+        public int minMiddleDistance { get; set; }
+    }
+}
